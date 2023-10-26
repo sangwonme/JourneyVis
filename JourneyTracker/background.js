2e8a6255c6c50e7f110658291b4080cd10e4977a
sangwonme <paulmoguri@naver.com>
2023-10-26 21:28:59 +0900

    ADD Get search term and search year

--- JourneyTracker/background.js
+++ JourneyTracker/background.js
@@ -4,10 +4,20 @@ chrome.webNavigation.onCommitted.addListener(
         if (details.url.includes("https://scholar.google.")) {
             let url = new URL(details.url);
             let searchTerm = url.searchParams.get("q");
+            let searchYearStart = url.searchParams.get("as_ylo");
+            let searchYearEnd = url.searchParams.get("as_yhi");
             if (searchTerm) {
                 console.log("Google Scholar Search Term:", searchTerm);
-                // Store or use the searchTerm as required
             }
+            if (searchYearEnd) {
+                console.log("Google Scholar Search Year:", searchYearStart + "~" + searchYearEnd);
+            }
+            else if (searchYearStart) {
+                console.log("Google Scholar Search Year:", searchYearStart);
+            }
+
+            // TODO : save to DB
+
         }
     },
     {urls: [
