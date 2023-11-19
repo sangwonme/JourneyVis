828b65bc09a47431c29aae0853c55b34f9f42b8e
sangwonme <paulmoguri@naver.com>
2023-11-19 19:58:31 +0900

    :sparkles: Add tree in the App.js

---
+++ client/src/components/ControlPanel.js
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect, useState} from "react";
+
+const ControlPanel = (props) => {
+
+	return (
+		<>
+    </>
+	);
+};
+
+export default ControlPanel;
---
+++ client/src/components/TreeChart.js
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect, useState} from "react";
+
+const TreeChart = (props) => {
+
+	return (
+		<>
+    </>
+	);
+};
+
+export default TreeChart;
