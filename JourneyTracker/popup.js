09a2aa1c36f671bebbfbb719e1a16bae3e30c931
sangwonme <paulmoguri@naver.com>
2023-11-19 14:39:10 +0900

    :bug: Fix writing ',' in csv

--- JourneyTracker/popup.js
+++ JourneyTracker/popup.js
@@ -2,22 +2,30 @@ document.getElementById("resetBtn").addEventListener("click", function() {
   chrome.storage.local.set({ searchLogs: [] });
 });
 
-
 document.getElementById("exportBtn").addEventListener("click", function() {
   chrome.storage.local.get({ searchLogs: [] }, function (result) {
       let csvContent = "data:text/csv;charset=utf-8,";
-      csvContent += "logtype,"
-      csvContent += "title,"
-      csvContent += "query,"
-      csvContent += "startYear,"
-      csvContent += "endYear,"
-      csvContent += "citedBy,"
-      csvContent += "authorID,"
-      csvContent += "cluster,"
-      csvContent += "url,"
-      csvContent += "Timestamp\r\n"; // CSV Header
+      csvContent += "\"logtype\","
+      csvContent += "\"title\","
+      csvContent += "\"query\","
+      csvContent += "\"startYear\","
+      csvContent += "\"endYear\","
+      csvContent += "\"citedBy\","
+      csvContent += "\"authorID\","
+      csvContent += "\"cluster\","
+      csvContent += "\"url\","
+      csvContent += "\"Timestamp\"\r\n"; // CSV Header
       result.searchLogs.forEach(function(log) {
-          let row = `${log.logtype},${log.title},${log.query},${log.yearStart},${log.yearEnd},${log.citedBy},${log.authorID},${log.cluster},${log.url},${log.timestamp}`;
+          let row = `"${escapeCSV(log.logtype)}",`
+                   + `"${escapeCSV(log.title)}",`
+                   + `"${escapeCSV(log.query)}",`
+                   + `"${escapeCSV(log.yearStart)}",`
+                   + `"${escapeCSV(log.yearEnd)}",`
+                   + `"${escapeCSV(log.citedBy)}",`
+                   + `"${escapeCSV(log.authorID)}",`
+                   + `"${escapeCSV(log.cluster)}",`
+                   + `"${escapeCSV(log.url)}",`
+                   + `"${escapeCSV(log.timestamp)}"`;
           csvContent += row + "\r\n";
       });
 
@@ -29,3 +37,12 @@ document.getElementById("exportBtn").addEventListener("click", function() {
       link.click();
   });
 });
+
+function escapeCSV(text) {
+  // Check if text is null or undefined
+  if (text === null || text === undefined) {
+      return '';
+  }
+  // Convert text to string in case it's not and escape double quotes
+  return String(text).replace(/"/g, '""');
+}
