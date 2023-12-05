a58682f3744a8b706fcc0bd20ef2edf61936af00
sangwonme <paulmoguri@naver.com>
2023-12-05 16:40:56 +0900

    :sparkles: Add Layout scss

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -3,7 +3,14 @@ import styles from './Layout.module.scss';
 
 const Layout = () => {
   return ( <>
-  
+  <div className={styles.layout}>
+    <div className={styles.left}></div>
+    <div className={styles.mid}>
+      <div className={styles.midUp}></div>
+      <div className={styles.midDown}></div>
+    </div>
+    <div className={styles.right}></div>
+  </div>
   </> );
 }
  
